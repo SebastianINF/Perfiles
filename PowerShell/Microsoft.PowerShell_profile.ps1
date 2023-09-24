b4fb07502d77fcf2dfafc578580dcfaeae8aa00c
Sebastian.developer.js <135476394+SebastianINF@users.noreply.github.com>
2023-09-24 18:20:36 -0400

    Delete PowerShell directory

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,3 +0,0 @@
-(@(& 'C:/Users/HP/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\HP\AppData\Local\Programs\oh-my-posh\themes\clean-detailed.omp.json' --print) -join "`n") | Invoke-Expression
-Import-Module -Name Terminal-Icons
-Set-PSReadLineOption -PredictionViewStyle ListView
